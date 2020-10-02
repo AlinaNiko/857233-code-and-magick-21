9ae8a5d29833e062e4985546b4bf6b2a01798607
AlinaNiko <alinanikolaenko91@gmail.com>
2020-10-02 09:07:49 +0300

    Исправлены функции генерации массива и отрисовки персонажей, исправлены имена функций

--- js/setup.js
+++ js/setup.js
@@ -5,7 +5,6 @@ const WIZARD_SURNAMES = [`да Марья`, `Верон`, `Мирабелла`, 
 const WIZARD_COAT_COLORS = [`rgb(101, 137, 164)`, `rgb(241, 43, 107)`, `rgb(146, 100, 161)`, `rgb(56, 159, 117)`, `rgb(215, 210, 55)`, `rgb(0, 0, 0)`];
 const WIZARD_EYES_COLORS = [`black`, `red`, `blue`, `yellow`, `green`];
 const WIZARD_NUMBER = 4;
-const wizardArray = [];
 
 const wizardContainer = document.querySelector(`.setup-similar-list`);
 const wizardTemplate = document.querySelector(`#similar-wizard-template`).content;
@@ -16,52 +15,56 @@ const setupSimilarBlock = setupBlock.querySelector(`.setup-similar`);
 setupBlock.classList.remove(`hidden`);
 setupSimilarBlock.classList.remove(`hidden`);
 
-const randomInteger = function (min, max) {
+const getRandomInteger = function (min, max) {
   const randomNumber = min + Math.random() * (max + 1 - min);
   return Math.floor(randomNumber);
 };
 
-const randomArrayItem = function (array) {
-  const randomItem = array[randomInteger(0, array.length - 1)];
+const getRandomArrayItem = function (array) {
+  const randomItem = array[getRandomInteger(0, array.length - 1)];
   return randomItem;
 };
 
-const fillWizardArray = function (array) {
+const generateWizards = function () {
+  const wizardArray = [];
   for (let i = 0; i < WIZARD_NUMBER; i++) {
-    const randomWizardFullName = randomArrayItem(WIZARD_NAMES) + ` ` + randomArrayItem(WIZARD_SURNAMES);
-    const randomWizardCoatColor = randomArrayItem(WIZARD_COAT_COLORS);
-    const randomWizardEyesColor = randomArrayItem(WIZARD_EYES_COLORS);
+    const randomWizardFullName = getRandomArrayItem(WIZARD_NAMES) + ` ` + getRandomArrayItem(WIZARD_SURNAMES);
+    const randomWizardCoatColor = getRandomArrayItem(WIZARD_COAT_COLORS);
+    const randomWizardEyesColor = getRandomArrayItem(WIZARD_EYES_COLORS);
     const object = {
       name: randomWizardFullName,
       coatColor: randomWizardCoatColor,
       eyeColor: randomWizardEyesColor
     };
-
-    array.push(object);
+    wizardArray.push(object);
   }
+  return wizardArray;
 };
 
-const createWizardElement = function (object) {
-  const wizardElement = wizardItem.cloneNode(true);
-  const nameElement = wizardElement.querySelector(`.setup-similar-label`);
-  const coatElement = wizardElement.querySelector(`.wizard-coat`);
-  const eyesElement = wizardElement.querySelector(`.wizard-eyes`);
+const createWizard = function (object) {
+  const wizard = wizardItem.cloneNode(true);
+  const name = wizard.querySelector(`.setup-similar-label`);
+  const coat = wizard.querySelector(`.wizard-coat`);
+  const eyes = wizard.querySelector(`.wizard-eyes`);
 
-  nameElement.textContent = object.name;
-  coatElement.style.fill = object.coatColor;
-  eyesElement.style.fill = object.eyeColor;
+  name.textContent = object.name;
+  coat.style.fill = object.coatColor;
+  eyes.style.fill = object.eyeColor;
 
-  return wizardElement;
+  return wizard;
 };
 
-const renderWizard = function (array) {
-  fillWizardArray(array);
+const renderWizards = function (array) {
+  const fragment = document.createDocumentFragment();
 
   for (let i = 0; i < array.length; i++) {
     const arrayItem = array[i];
-    const readyWizardElement = createWizardElement(arrayItem);
-    wizardContainer.appendChild(readyWizardElement);
+    const readyWizard = createWizard(arrayItem);
+    fragment.appendChild(readyWizard);
   }
+
+  wizardContainer.appendChild(fragment);
 };
 
-renderWizard(wizardArray);
+const wizards = generateWizards();
+renderWizards(wizards);
